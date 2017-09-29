993dbfbc8e5f09fac9d8d026fb97403b084215fb
avinash <avinash@hebecollins.com>
2017-09-29 18:59:52 +0530

    flash message rendering in a component and delete them

--- client/components/App.js
+++ client/components/App.js
@@ -1,37 +1,14 @@
 import React from 'react';
 import Greetings from './Greetings';
 import NavigationBar from './NavigationBar';
-import IntlTelInput from 'react-intl-tel-input';
-import 'file?name=libphonenumber.js!./../../node_modules/react-intl-tel-input/dist/libphonenumber.js';
-import './../../node_modules/react-intl-tel-input/dist/main.css';
+import FlashMessageList from './flash/FlashMessageList'
 
-class App extends React.Component{
-
-    handle(status, value, countryData, number){
-        console.log(
-            // status// cam be used for validation
-            // , value
-             countryData.dialCode
-             //  number
-        );
-    }
-
+class App extends React.Component {
     render() {
-        // const handle = (status, value, countryData, number) => {
-        //     console.log( number);
-        //
-        // };
-        return(
+        return (
             <div className="container">
                 <NavigationBar/>
-                <IntlTelInput
-                    preferredCountries={['in']}
-                    onPhoneNumberChange={this.handle }
-                    placeholder = {'9876543210'}
-                    numberType="MOBILE"
-                    css={ ['intl-tel-input', 'form-control'] }
-                    utilsScript={ 'libphonenumber.js' }
-                />
+                <FlashMessageList/>
                 {this.props.children}
             </div>
         );
