2273a30d39162ea900b941fc5edffddbe351b500
avinash kumar <avinash@qburst.com>
2017-10-25 15:44:33 +0530

    day change fix

--- app/components/hebecollins/trainer/AddWorkout.js
+++ app/components/hebecollins/trainer/AddWorkout.js
@@ -20,35 +20,40 @@ class AddWorkout extends React.Component {
 
     onNext() {
         console.log(this.state.week);
+        this.setState({index:this.state.index+1})
     }
 
     onPrevious() {
-
+        this.setState({index:this.state.index-1})
     }
 
     render() {
         const {dayName, week, index} = this.state;
         const dayOfWeek = (i) => {
-            if (i < 0)
-                return 7 + i;
-            else if (i > 6)
-                return i - 7;
+            if (i%7 < 0)
+                return 7 + i%7;
             else
-                return i
+                return i%7;
         };
+
         return (
             <div className="content">
                 <SingleScreen>
+
+                    <h1 className="white-center">{dayName[dayOfWeek(index)]}</h1>
+
                     <WorkoutGroup week={week} dayOfWeek={dayName[dayOfWeek(index)]}/>
 
                     <div className="pager">
-                        <button onClick={this.onNext}
-                                className="btn-next btn-hebecollins">{dayName[dayOfWeek(index + 1)]}
+                        <button onClick={this.onPrevious}
+                                className="btn-hebecollins-black">{dayName[dayOfWeek(index - 1)]}
                         </button>
                         <button onClick={this.onNext}
-                                className="btn-next btn-hebecollins">{dayName[dayOfWeek(index - 1)]}
+                                className="btn-hebecollins-black">{dayName[dayOfWeek(index + 1)]}
                         </button>
                     </div>
+
+                    <h1 className="white-center">{dayName[dayOfWeek(index)]}</h1>
                 </SingleScreen>
             </div>
         )
